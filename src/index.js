af09799bd79b1e41bfec0a726e84e05f1f39fec2
JihyeSeo1120 <mansesjh22@gmail.com>
2020-01-22 17:44:21 +0900

    디비 연결

---
+++ src/index.js
@@ -0,0 +1,26 @@
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+
+const {
+    PORT : port = 3000,
+    MONGO_URI : mongoURI
+} = process.env;
+
+mongoose.Promise = global.Promise;
+mongoose.connect(mongoURI, {useNewUrlParser: true, useUnifiedTopology:true}).then(()=>{
+    console.log('connect to mongodb');
+}).catch((e)=>{
+    console.log(e);
+});
+const app = express();
+
+app.use('/api', require('./api'));
+
+
+app.get('/', (req, res)=> {
+    res.send('성공');
+})
+app.listen(port,()=>{
+    console.log(`listening on port ${port}`);
+});
