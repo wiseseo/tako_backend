73732bed02fb86fac5ad4802dfc8cb8a66ade9a5
JihyeSeo1120 <mansesjh22@gmail.com>
2020-01-24 16:14:52 +0900

    메뉴 등록시 내가게 등록까지 연결

--- src/api/store/index.js
+++ src/api/store/index.js
@@ -1,31 +1,31 @@
 const express = require('express');
 const router = express.Router();
 const Stores = require('../../models/stores');
+const Users = require('../../models/users');
 
 router.use(express.json());
 
 router.get('/', (req,res)=>{
     res.send('store page');
 });
-
+//가게 등록
 router.post('/', (req,res)=> {
     console.log(req.body);
     const {title, type, address, time, description} = req.body;
-    console.log(Stores.create({title, type, location : { address }, time, description}));
     const promise = Stores.create({title, type, location : { address }, time, description});
     promise.then((store)=>{
-       console.log(store);
+       //console.log(store);
        const storeId = store._id;
-       console.log(storeId);
+       //console.log(storeId);
        return storeId;
        
     })
     res.send('register store');
 });
-
+//메뉴 등록
 router.patch('/:storeId/menu', async (req,res) => {
     const storeId = req.params.storeId;
-    const {menu, price, photo} = req.body;
+    const {menu, price, photo, userId} = req.body;
     //console.log(storeId);
     //console.log(await Stores.findById(storeId));
     const store = await Stores.findById(storeId);
@@ -34,9 +34,16 @@ router.patch('/:storeId/menu', async (req,res) => {
         price,
         photo
     });
-    console.log(store);
-    Stores.findByIdAndUpdate(storeId,store);
-    res.send('..');
-})
+    //console.log(store);
+    await Stores.findByIdAndUpdate(storeId,store);
+    //내가게등록까지
+    const user = await Users.findOne({id : userId});
+    user.stores.push(store._id);
+    //console.log(user);
+    await Users.findOneAndUpdate({id:userId},user);
+    res.send('register menu');
+});
+
+
 module.exports = router;
 
