fab9dcd8ac0560ff1389a388d3f1599b5bdf75d9
JihyeSeo1120 <mansesjh22@gmail.com>
2020-01-25 16:53:25 +0900

    내가게등록을 가게등록으로

--- src/api/store/index.js
+++ src/api/store/index.js
@@ -11,23 +11,21 @@ router.get('/', (req,res)=>{
 //가게 등록
 router.post('/', (req,res)=> {
     console.log(req.body);
-    const {title, type, address, time, description} = req.body;
+    const {title, type, address, time, description, userId} = req.body;
     //address에서 latitude, longtitude 로 변환 필요
     const promise = Stores.create({title, type, location : { address }, time, description});;
-    promise.then((store)=>{
+    promise.then(async (store)=>{
        const storeId = store._id;
-       return storeId;
-       
-    })
-    res.send('register store');
+       //내가게등록
+       await Users.findOneAndUpdate({id:userId},{$push:{stores: storeId}});
+       res.send(storeId);
+    });
 });
 //메뉴 등록
 router.patch('/:storeId/menu', async (req,res) => {
     const storeId = req.params.storeId;
-    const {menu, price, photo, userId} = req.body;
+    const {menu, price, photo} = req.body;
     await Stores.findByIdAndUpdate(storeId,{$push : {items : {menu, price, photo}}});
-    //내가게등록까지
-    await Users.findOneAndUpdate({id:userId},{$push:{stores: storeId}});
     res.send('register menu');
 });
 
